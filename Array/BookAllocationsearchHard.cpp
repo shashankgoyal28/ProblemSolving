f495f94a59a714f18ec96ce9a88d9adb3db37cac
shashank <shashankaggarwal654@gmail.com>
2025-12-16 19:36:44 +0530

    Evening Practice

---
+++ Array/BookAllocationsearchHard.cpp
@@ -0,0 +1,19 @@
+// Book Allocation Problem
+// There are N books, each "i"th book has A[i] number of pages.
+// You have to allocate books to M number of students so that 
+// the maximum number of pages allocated to a student is minimum.
+
+// Note:
+// • Each book should be allocated to a student.
+// • Each student has to be allocated at least one book.
+// • Allotment should be in contiguous order.
+
+// Calculate and return that minimum possible number.
+// Return -1 if a valid assignment is not possible.
+
+#include<iostream>
+using namespace std;
+int main(){
+
+return 0;
+}
