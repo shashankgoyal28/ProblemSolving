a1cec0967b426d1cef726614bf511f7b8427cf75
shashank <shashankaggarwal654@gmail.com>
2026-01-04 14:41:46 +0530

    v1.4

---
+++ Sorting Techniques/Mergesort.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<vector> 
+using namespace std;
+// this is a Classic Example of Divide and Conquer Algorithm using Recursion 
+void mergeSort(vector<int>& arr, int start, int end)
+{
+     // base case
+    if(start >= end)
+    {
+        return;
+    }
+    if(start < end){
+    int mid = start + (end - start) / 2;
+    // left part -> Recursive Call
+    mergeSort(arr, start, mid);
+    // right part -> Recursive Call
+    mergeSort(arr, mid + 1, end);
+    return ;
+    }
+}
+void Merge(vector<int>& arr, int start, int mid, int end)
+{
+    vector<int> temp;
+    int i = start; // left part
+    int j = mid + 1; // right part
+
+    while(i <= mid && j <= end)
+    {
+        if(arr[i] <= arr[j])
+        {
+            temp.push_back(arr[i]);
+            i++;
+        }
+        else {
+            temp.push_back(arr[j]);
+            j++;
+        }
+    } 
+}
+
+int main(){
+    vector<int> arr = {38, 27, 43, 3, 9, 82, 10};
+mergeSort(arr, 0, arr.size() - 1);
+return 0;
+}
