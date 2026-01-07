ba8a5806dbbff4243fc733014f842dbdffe4a397
shashank <shashankaggarwal654@gmail.com>
2026-01-07 09:53:53 +0530

    v1.7

--- Linkedlist/DoublyLinkedList.cpp
+++ Linkedlist/DoublyLinkedList.cpp
@@ -1,6 +1,27 @@
 #include<iostream>
 using namespace std;
-int main(){
 
+class Node{
+    public:
+    int data;
+    Node* next;
+    Node* prev;
+    Node(int val){
+        data=val;
+        next=NULL;
+        prev=NULL;
+    }
+};
+class DoublyLinkedList{
+    public:
+    Node* head;
+    Node* tail;
+    DoublyLinkedList(){
+        head = NULL;
+        tail = NULL;
+    }
+};
+int main(){
+DoublyLinkedList dll;
 return 0;
 }
