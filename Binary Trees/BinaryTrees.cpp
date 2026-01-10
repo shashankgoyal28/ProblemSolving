f410a686fd073e2c659970401c9fc893c6c58987
shashank <shashankaggarwal654@gmail.com>
2026-01-10 21:39:36 +0530

    v1.10

---
+++ Binary Trees/BinaryTrees.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val)
+    {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+static int idx = -1;
+Node* buildTree(vector<int> &preorder){
+    idx++;
+    if(preorder[idx] == -1){
+        return NULL;
+    }
+    Node* root = new Node(preorder[idx]);
+    root-> left = buildTree(preorder);
+    root-> right = buildTree(preorder);
+    return root;
+}
+int main(){
+vector<int> preOrder = {1,2,-1,-1,3,4,-1,-1,5,-1,-1};
+Node* root = buildTree(preOrder);
+cout << "Root node is: " << root->data << endl;
+return 0;
+}
