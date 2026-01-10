36ee122da34914b6473170dc86dcb0223d11887a
shashank <shashankaggarwal654@gmail.com>
2026-01-10 22:59:26 +0530

    v1.10

---
+++ Binary Trees/HeightofTree.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val)
+    {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+static int idx = -1;
+Node* buildTree(vector<int> &preorder){
+    idx++;
+    if(preorder[idx] == -1){
+        return NULL;
+    }
+    Node* root = new Node(preorder[idx]);
+    root-> left = buildTree(preorder);
+    root-> right = buildTree(preorder);
+    return root;
+}
+
+int heightofTree(Node* root){
+if(root == NULL){
+    return 0;
+}
+int leftHeight = heightofTree(root-> left);
+int rightHeight = heightofTree(root-> right);
+return max(leftHeight, rightHeight) + 1; //+1 is used to add the root node.
+}
+
+int main(){
+vector<int> preOrder = {1,2,-1,-1,3,4,-1,-1,5,-1,-1};
+Node* root = buildTree(preOrder);
+
+cout<< endl;
+cout << "Root node is: " << root->data << endl;
+return 0;
+}
