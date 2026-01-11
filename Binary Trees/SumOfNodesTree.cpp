3476039a6d470b2f26722b7ba50f6eff2c42c2ec
shashank <shashankaggarwal654@gmail.com>
2026-01-11 11:00:57 +0530

    v1.11

---
+++ Binary Trees/SumOfNodesTree.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val)
+    {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+static int idx = -1;
+Node* buildTree(vector<int> &preorder){
+    idx++;
+    if(preorder[idx] == -1){
+        return NULL;
+    }
+    Node* root = new Node(preorder[idx]);
+    root-> left = buildTree(preorder);
+    root-> right = buildTree(preorder);
+    return root;
+}
+
+int SumofTree(Node* root){
+if(root == NULL){
+    return 0;
+}
+int leftSum = SumofTree(root-> left);
+int rightSum = SumofTree(root-> right);
+return leftSum + rightSum + root->data;
+}
+
+int main(){
+vector<int> preOrder = {1,2,-1,-1,3,4,-1,-1,5,-1,-1};
+Node* root = buildTree(preOrder);
+cout << "Sum of nodes in the tree is: " << SumofTree(root) << endl;
+return 0;
+}
