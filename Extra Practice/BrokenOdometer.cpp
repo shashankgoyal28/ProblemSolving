64a21bc11b1d18b46fa933ec6ee803408c0055f7
shashank <shashankaggarwal654@gmail.com>
2026-01-28 12:40:33 +0530

    V1.28

--- Extra Practice/BrokenOdometer.cpp
+++ Extra Practice/BrokenOdometer.cpp
@@ -1,6 +1,19 @@
-#include<iostream>
+#include <iostream>
+#include <string>
 using namespace std;
-int main(){
-
-return 0;
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        string n;
+        cin >> n;
+        long long ans = 0;
+        for (char c : n) {
+            int d = c - '0';
+            if (d > 3) d--;   // skip digit 3
+            ans = ans * 9 + d;
+        }
+        cout << ans << endl;
+    }
+    return 0;
 }
