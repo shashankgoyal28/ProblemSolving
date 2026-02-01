047576ea2ab8ae1e9ffc3233cb62fdebd07aa846
shashank <shashankaggarwal654@gmail.com>
2026-02-01 12:05:20 +0530

    First commit of this month v2.1

--- Dynamic Programming/0/knapsack01Memo.cpp
+++ Dynamic Programming/0/knapsack01Memo.cpp
@@ -1,13 +1,14 @@
 #include<iostream>
 using namespace std;
 
-int memo[1001][1001];
+int static memo[1001][1001]; // creating a memoization table or a matrix
 int knapsack(int wt[], int val[], int w, int n)
 {
     if(n == 0 || w == 0)
     {
         return 0;
     }
+    // check if the value is already computed
     if(memo[n][w] != -1)
     {
         return memo[n][w];
@@ -28,7 +29,7 @@ int knapsack(int wt[], int val[], int w, int n)
 
 int main()
 {
-    memset(memo, -1, sizeof(memo));
+    memset(memo, -1, sizeof(memo)); // initializing the memoization table with -1
     int wt[]  = {1, 3, 4, 5};
     int val[] = {1, 4, 5, 7};
 
