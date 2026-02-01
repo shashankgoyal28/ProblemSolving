cd35cc5c03ed73d5c8acdfbb6a12c6c5961e949c
shashank <shashankaggarwal654@gmail.com>
2026-02-01 14:19:43 +0530

    First commit of this month v2.1

--- Dynamic Programming/0/knapsack01topDownApproach.cpp
+++ Dynamic Programming/0/knapsack01topDownApproach.cpp
@@ -1,6 +1,37 @@
 #include<iostream>
 using namespace std;
+int dp[1001][1001]; // creating a 2D array for dynamic programming
+int knapsack(int wt[], int val[], int w, int n)
+{
+    for(int i = 0; i <= n;i++)
+    {
+        for(int j = 0; j <= w; j++)
+        {
+            if(i == 0 || j == 0)
+            {
+                // base case 
+                dp[i][j] = 0;
+            }
+            if(wt[i-1] <= j)
+            {
+                dp[i][j] = max(val[i-1] + dp[i-1][j - wt[i-1]], dp[i-1][j]);
+            }
+            else{
+                dp[i][j] = dp[i-1][j];
+            }
+        }
+    }
+    return dp[n][w];
+}
 int main(){
+    int wt[]  = {1, 3, 4, 5};   // weights
+    int val[] = {1, 4, 5, 7};   // values
 
+    int w = 7;                 // knapsack capacity
+    int n = sizeof(wt) / sizeof(wt[0]);  // number of items
+
+    int maxProfit = knapsack(wt, val, w, n);
+
+    cout << "Maximum value in knapsack = " << maxProfit << endl;
 return 0;
 }
