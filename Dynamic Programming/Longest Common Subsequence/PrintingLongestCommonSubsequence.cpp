e6d057e96607aba5a25923be5e1e5b2f4d7a6fa2
shashank <shashankaggarwal654@gmail.com>
2026-02-17 16:07:18 +0530

    v2.17

--- Dynamic Programming/Longest Common Subsequence/PrintingLongestCommonSubsequence.cpp
+++ Dynamic Programming/Longest Common Subsequence/PrintingLongestCommonSubsequence.cpp
@@ -1,6 +1,15 @@
 #include<iostream>
 using namespace std;
-int main(){
+string printingLongestCommonSubsequence(string s1, string s2){
 
+}
+int main(){
+    string s1, s2;
+    cout<<"Enter first string:";
+    cin>>s1;
+    cout<<"Enter second string:";
+    cin>>s2;
+    string result = printingLongestCommonSubsequence(s1, s2);
+    cout<<"Longest Common Subsequence is: "<<result<<endl;
 return 0;
 }
