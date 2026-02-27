67dd7da7ed8aac7528c730ae284cb1e89e930189
shashank <shashankaggarwal654@gmail.com>
2026-02-27 13:04:53 +0530

    ComPt

--- Practice/MinimumNoOfTransactions.cpp
+++ Practice/MinimumNoOfTransactions.cpp
@@ -2,28 +2,37 @@
 #include<vector>
 #include<queue>
 using namespace std;
-
-vector<int> slidingWindowMinimumNoOfTransactions(vector<int>& arr, int k)
+vector<int>slidingWindowMinimumNoofTransactions(vector<int> arr, int k)
 {
-    int n = arr.size();
     vector<int> result;
-    priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
-
-    for(int i = 0; i < n; i++)
+    int n = arr.size();
+    // Min Heap consist of pairs of (value, index)
+    priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> min_heap;
+    for(int i = 0; i< n;i++)
     {
-        pq.push({arr[i], i});
-        while(pq.top().second <= i - k)
-        {
-            pq.pop();
-        }
+        min_heap.push({arr[i], i});
 
+        while(min_heap.top().second <= i - k)
+        {
+            min_heap.pop();
+        }
         if(i >= k - 1)
         {
-            result.push_back(pq.top().first);
+            result.push_back(min_heap.top().first);
         }
     }
-
     return result;
+}
+int main(){
+    vector<int> arr = {10, 20, 30, 50, 10, 70, 30};
+    int k = 3;
+    vector<int> result = slidingWindowMinimumNoofTransactions(arr, k);
+    for(int i : result)
+    {
+        cout << i << " ";
+    }
+
+return 0;
 }
 
         // second -> index of the element in the array
