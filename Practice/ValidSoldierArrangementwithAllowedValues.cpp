12354dfec45786b3db5159c768d3b642e23282d7
shashank <shashankaggarwal654@gmail.com>
2026-03-24 22:29:22 +0530

    V3.24

--- Practice/ValidSoldierArrangementwithAllowedValues.cpp
+++ Practice/ValidSoldierArrangementwithAllowedValues.cpp
@@ -1,7 +1,36 @@
-#include<iostream>
+#include <iostream> 
 using namespace std;
+void solve(int index, vector<int>& arr, vector<int>& values, int& count, int n) {
+    if(index == n - 1) {
+        if(arr[index] != arr[index - 1]) {
+            count++;
+        }
+        return;
+    }
+    for(int val : values) {
+        if(val != arr[index - 1]) { // pruning
+            arr[index] = val;
+            solve(index + 1, arr, values, count, n);
+        }
+    }
+}
 
-int main(){
+int countWays(int n, vector<int>& values, int end) {
+    vector<int> arr(n);
+    arr[0] = 1;
+    arr[n - 1] = end;
+    int count = 0;
+    solve(1, arr, values, count, n);
+    return count;
+}
 
-return 0;
+int main() {
+    int n = 4;
+    vector<int> values = {1,2,3,4};
+    int end = 3;
+
+    int totalArrangements = countWays(n, values, end);
+    cout << "Total Valid Arrangements: " << totalArrangements << endl;
+
+    return 0;
 }
