be50565ff752a3b96bb45c9dbeb61c36299f7450
shashank <shashankaggarwal654@gmail.com>
2026-03-25 21:05:16 +0530

    V3.24

--- Practice/JohnTheMathematicsTeacher.cpp
+++ Practice/JohnTheMathematicsTeacher.cpp
@@ -1,7 +1,68 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
 using namespace std;
 
-int main(){
+int main()
+{
+    int n, k;
+    cout << "Enter the Value of N and K" << endl;
+    cin >> n >> k;
 
-return 0;
+    vector<int> M(n);
+    cout << "Enter the Marks of the Students" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> M[i];
+    }
+
+    int breaks = 0;
+    for (int i = 1; i < n; i++)
+    {
+        if (M[i] != M[i - 1])
+        {
+            breaks++;
+        }
+    }
+
+    while (k--)
+    {
+        int X, Y;
+        cout << "Enter the Value of X and Y" << endl;
+        cin >> X >> Y;
+
+        X--;
+        if (X > 0)
+        {
+            if (M[X] != M[X - 1])
+            {
+                breaks--;
+            }
+        }
+        if (X < n - 1)
+        {
+            if (M[X] != M[X + 1])
+            {
+                breaks--;
+            }
+        }
+        M[X] = Y;
+        if (X > 0)
+        {
+            if (M[X] != M[X - 1])
+            {
+                breaks++;
+            }
+        }
+        if (X < n - 1)
+        {
+            if (M[X] != M[X + 1])
+            {
+                breaks++;
+            }
+        }
+        cout << "The No. Of Students in the Merit List were : " << breaks + 1 << endl;
+    }
+
+    return 0;
 }
